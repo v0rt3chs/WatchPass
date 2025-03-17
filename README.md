710a62e103e90f10a8ab7d4742e7ca8dfed8cbcc
v0rt3chs <140927994+v0rt3chs@users.noreply.github.com>
2025-03-17 13:58:26 -0500

    Initial upload of WatchPass
    Finally moved it to GitHub. WatchPass started as a script for watching for password resets, and slowly developed into a PowerShell utility that quickly contains O365 and Active Directory Identity incidents including account compromise.

---
+++ README.md
@@ -0,0 +1,77 @@
+
+## GitHub Repository
+
+At this time, there is no GitHub repo for `WatchPass`. If I do choose to create a repository for it, you will find it on my [GitHub](https://www.github.com/v0rt3chs?tab=repositories)
+
+## Overview
+
+`WatchPass` was originally written during a time where account compromises were fairly frequent. Its primary functionality was to monitor an account for a password reset from the Support team. Over time, I started adding more functionality to it that would help us automate the process of containing O365 account compromise (and other identity) incidents ourselves.
+
+## Prerequisites
+
+- VPN or on-premises network connectivity for password resets in on-prem AD
+- AzureAD PowerShell Module
+- ActiveDirectory PowerShell Module
+- Authentication Administrator or Privileged Authentication Administrator depending on privileges of impacted user
+- Privileges to reset passwords in on-prem AD
+
+## Basic Usage
+
+- Watch for password reset for user at set interval of 30 seconds
+```powershell
+.\WatchPass.ps1 -user cdawkins -refresh 30
+```
+If no refresh rate is specified, the default will be 120 seconds. After one hour, you will be asked if you would like `WatchPass` to reset the password for you. Doing password reset this way does *not* revoke sessions. The `-verbose` switch can be added to get updates at each `refresh` interval. If you are wanting to take a screenshot for documentation, leaving verbose out might be best in this case.
+
+- Reset user password
+```powershell
+.\WatchPass.ps1 -user cdawkins -reset
+```
+When `WatchPass` does a password reset, it prioritizes the Entra password reset first. It will try grabbing the UPN from on-prem AD right away though, so you may be prompted to manually input the user's email address if on-prem network connectivity cannot be established. As long as you have proper permissions in Entra, the Entra password reset will work smoothly. Then the On-prem password reset will kickoff using the same password sample. As long as you have permissions to reset the user's password and on-prem network connectivity, the password reset should work fine. As always the `-verbose` switch may be included to provide more details.
+
+- Revoke user sessions
+```powershell
+.\WatchPass.ps1 -user cdawkins -revoke
+```
+There may be times where you only want to revoke a user's sessions. This may be preferred after a user's password has already been reset by another IT department member. `WatchPass` will try to establish *your* email address using on-prem AD, if for some reason you are not in the office or on the on-prem network, you will need to enter your own email address. This is used to connect you to the AzureAD PowerShell module. Another check for the user's UPN will occur on-prem, you may have to manually enter the email address if `WatchPass` cannot get it for you. The `-verbose` switch can be added for status updates and more info.
+
+- Contain user account
+```powershell
+.\WatchPass.ps1 -user cdawkins -contain
+```
+Many times you will want to both reset the password and revoke user sessions to fully contain an account compromise. Passing the `-contain` switch will function the same as passing both the `-reset` and `-revoke` switches together. The password resets will always be processed before containment.
+
+## Parameters
+
+- `-user` - this is a required parameter in which you pass the username of the user you are performing actions on (do not include @domain.whatever).
+
+- `-refresh` - this is an optional parameter for watching for password resets. This defines the rate (in seconds) at which `WatchPass` will check on-prem AD for a changed password. If not set during a password watch, the default value is 120 seconds. This value cannot be higher than 3600 seconds. If you set it higher, it will automatically default back to 120 seconds.
+
+- `-reset` - this is an optional parameter that tells `WatchPass` to try to reset the user's password in both Entra and on-premises AD.
+
+- `-revoke` - this is an optional parameter that tells `WatchPass` to try to revoke the user's sessions in Entra.
+
+- `-contain` - this is an optional parameter that tells `WatchPass` to try and reset and revoke user sessions. It combines the functionality of `-reset` and `-revoke` parameters.
+
+- `-verbose` - this is an optional parameter that provides more information for the admin running `WatchPass`. This can be beneficial for troubleshooting errors or providing more context in screenshots. In certain cases though such as watching for password resets, it could make your screenshot much larger as it will print a line for each check depending on `-refresh` value.
+
+## Other notes
+
+- `WatchPass` password resets are conducted based on the zero knowledge concept. The plaintext sample generated for a password is a 64 character string including uppercase letters, lowercase letters, numbers, and symbols. This plaintext sample string is never printed to the console or stored permanently anywhere else on the system. As soon as the plaintext sample is generated, a Microsoft secure string is created using it. Immediately after the secure string is created, the plaintext sample string is destroyed and deallocated in memory. After both password resets are done, or if a terminating error is reached during password reset attempts, the secure string is also destroyed and deallocated in memory. Resetting the password to something unknown is *intentional* as another administrator can simply reset the password again when they are ready to help the user get signed back in. This way, no one ever needs to know the password to make the account secure, which also eliminates the risk of insider threat until the password is reset again.
+
+- SHA-256 Hash of most current version of `WatchPass`
+```
+84481974C66875D79ECE6580A190F71EE6D276BE115F019FDE89090BBCE084C3
+```
+
+## Known Issues
+
+- In its current state, `WatchPass` has a heavy reliance on on-premises Active Directory. My intention was to fix this by separating AD and Azure (Entra) as much as possible, but I did not have enough time to do so before I left. I may continue development of this tool if there is any interest in it.
+
+- Password reset watching functionality currently only works for an hour before prompting for password reset/revoking sessions. Regardless of what you opt into or out of at this point, `WatchPass` will terminate without watching for password resets any longer. This could easily be fixed with a new switch.
+
+- If you exclude the `-verbose` switch from a normal password watch use case, you also forfeit your ability to have password reset and session token revocation with verbosity.
+
+- While watching for password resets, if the password is reset in such a way that does not create a "passwordlastset" date in ActiveDirectory, the output from the script will also not display a date for what time the password was reset. This may be a good use case for `-verbose` as you will have a timestamp right above it for the last check before the password was updated.
+
+- The ASCII art is currently printed each time the tool is run. If I continue development, I will likely add a `-nobanner` (or similar) switch to remove the ASCII art for conciseness and maximal "screenshotability."
---
+++ WatchPass.ps1
@@ -0,0 +1,227 @@
+﻿<# ============================================================ 
+Written By: Curtis Dawkins
+Date Created: 2023
+Last Updated: 3/11/2025
+Purpose: Streamline the account compromise containment process.
+============================================================ #> 
+
+#Command line args
+param($user,[int]$refresh,[switch]$verbose,[switch]$reset,[switch]$revoke,[switch]$contain)
+
+function Reset-Pass {
+    if ($verbose){
+        Write-Host "Trying to reset user's password..."`n -ForegroundColor Magenta
+    }
+    try{$upn = (get-aduser $Env:UserName).UserPrincipalName}
+    catch{
+        Write-Warning -Message "There was an error getting your email address. If you are not in the office or on the VPN, this is expected."
+        $upn= Read-Host "Please enter your email address: "
+    }
+    try {Connect-AzureAD -AccountId $upn | Out-Null}
+    catch{Write-Warning -Message "There was an error connecting to AzureAD Powershell. Make sure you have the AzureAD PowerShell module installed!"; exit}
+    try{$userUPN=(get-aduser $user).UserPrincipalName}
+    catch{
+        Write-Warning -Message "Could not get user's UPN automatically. If you are not in the office or on the VPN, this is expected. "
+        $userUPN= Read-Host "Please enter the user's email address: "
+    }
+    if ($verbose){
+        Write-Host "Resetting Azure password for"$userUPN"..."`n -ForegroundColor Magenta
+    }
+    $sample = -join ((65..90) + (97..122) + (33..64) | Get-Random -Count 64 | % {[char]$_})
+    $pswd = ConvertTo-SecureString $sample -AsPlainText -Force
+    $sample=$null
+    try{Set-AzureADUserPassword -ObjectId $userUPN -Password $pswd}
+    catch{Write-Warning "There was an error resetting the password. Are you an Authentication Administator or Privileged Authentication Administrator?"; $pswd=$null; exit}
+    Write-Host "Azure Password reset completed successfully at: "$((date))`n -ForegroundColor Green
+    sleep 2 #this is to avoid the password resets overlapping eachother
+    if ($verbose){
+        Write-Host "Resetting on-prem AD password for"$userUPN"..."`n -ForegroundColor Magenta
+    }
+    try{Set-ADAccountPassword -Identity $user -Reset -NewPassword $pswd}
+    catch{
+        Write-Warning "There was an error resetting the password in on-prem AD. Are you on the production network or VPN? Do you have permissions to reset this account's password?"
+        $pswd=$null
+        exit 
+    }
+    Write-Host "On-Prem AD Password reset completed successfully at: "$((date))`n -ForegroundColor Green
+    $pswd=$null
+}
+
+function Revoke-Sessions {
+    if ($verbose){
+        Write-Host "Trying to revoke user's sessions..."`n -ForegroundColor Magenta
+    }
+    try{$upn = (get-aduser $Env:UserName).UserPrincipalName}
+    catch{
+        Write-Warning -Message "There was an error getting your email address. If you are not in the office or on the VPN, this is expected. "
+        $upn= Read-Host "Please enter your email address: "
+    }
+    try {Connect-AzureAD -AccountId $upn | Out-Null}
+    catch{Write-Warning -Message "There was an error connecting to AzureAD Powershell. Make sure you have the AzureAD PowerShell module installed!"; exit}
+    try{$userUPN=(get-aduser $user).UserPrincipalName}
+    catch{
+        Write-Warning -Message "Could not get user's UPN automatically. If you are not in the office or on the VPN, this is expected. "
+        $userUPN= Read-Host "Please enter the user's email address: "
+    }
+    try{Revoke-AzureADUserAllRefreshToken -ObjectId $userUPN}
+    catch{
+        Write-Warning -Message "There was an error revoking the user's sessions. Are you an Authentication Administator or Privileged Authentication Administrator? "
+        exit
+    }
+    Write-Host "Sessions revoked successfully at: "$((date)) -ForegroundColor Green
+}
+
+function Contain{
+    if ($verbose){
+        Write-Host "Trying to reset user's password and revoke user's sessions..."`n -ForegroundColor Magenta
+    }
+    try{$upn = (get-aduser $Env:UserName).UserPrincipalName}
+    catch{
+        Write-Warning -Message "There was an error getting your email address. If you are not in the office or on the VPN, this is expected."
+        $upn= Read-Host "Please enter your email address: "
+    }
+    try {Connect-AzureAD -AccountId $upn | Out-Null}
+    catch{Write-Warning -Message "There was an error connecting to AzureAD Powershell. Make sure you have the AzureAD PowerShell module installed!"; exit}
+    try{$userUPN=(get-aduser $user).UserPrincipalName}
+    catch{
+        Write-Warning -Message "Could not get user's UPN automatically. If you are not in the office or on the VPN, this is expected. "
+        $userUPN= Read-Host "Please enter the user's email address: "
+    }
+    if ($verbose){
+        Write-Host "Resetting Azure password for"$userUPN"..."`n -ForegroundColor Magenta
+    }
+    $sample = -join ((65..90) + (97..122) + (33..64) | Get-Random -Count 64 | % {[char]$_})
+    $pswd = ConvertTo-SecureString $sample -AsPlainText -Force
+    $sample=$null
+    try{Set-AzureADUserPassword -ObjectId $userUPN -Password $pswd}
+    catch{Write-Warning "There was an error resetting the password in Azure. Are you an Authentication Administator or Privileged Authentication Administrator?"; exit}
+    Write-Host "Azure Password reset completed successfully at: "$((date))`n -ForegroundColor Green
+    #on-prem password reset
+    sleep 2 #this is to avoid the password resets overlapping eachother
+    if ($verbose){
+        Write-Host "Resetting on-prem AD password for"$userUPN"..."`n -ForegroundColor Magenta
+    }
+    try{Set-ADAccountPassword -Identity $user -Reset -NewPassword $pswd}
+    catch{
+        Write-Warning "There was an error resetting the password in on-prem AD. Are you on the production network or VPN?"
+        exit 
+    }
+    Write-Host "On-Prem AD Password reset completed successfully at: "$((date))`n -ForegroundColor Green
+
+    if ($verbose){
+        Write-Host "Revoking sessions for"$userUPN"..."`n -ForegroundColor Magenta
+    }
+    try{Revoke-AzureADUserAllRefreshToken -ObjectId $userUPN}
+    catch{
+        Write-Warning -Message "There was an error revoking the user's sessions. Are you an Authentication Administator or Privileged Authentication Administrator? "
+        exit
+    }
+    Write-Host "Sessions revoked successfully at: "$((date)) -ForegroundColor Green
+}
+
+#Error Checking
+if ($PSBoundParameters.ContainsKey('user')){
+    $start=date
+    write-host `n"User Passed: "
+    write-host "$((get-aduser $user).Name)" -ForegroundColor Magenta
+    write-host `n"Watch Pass was started at: "
+    write-host $start -ForegroundColor Magenta
+    if ($verbose){
+        write-host `n"verbose mode is enabled" -ForegroundColor Green
+    }
+    
+} else {
+    write-host "Usage: .\WatchPass.ps1 -user <username> [-refresh <refresh-rate-in-seconds>] [ -contain | -reset | -revoke | -verbose ]" -ForegroundColor Red
+    exit
+}
+
+#refresh rate
+if ($PSBoundParameters.ContainsKey('refresh') -and $refresh -le 3600){
+    $rr=$refresh
+} else {
+    if ($refresh -gt 3600){
+        write-host "Refresh value set higher than maximum (3600 seconds). Value has been set to default of 120 seconds." -ForegroundColor Yellow
+    }
+    $rr=120 #default refresh rate (in seconds) if none is specified
+}
+
+Write-Host `n$user"'s last password reset occurred: "
+$last=(Get-ADUser $user -properties *).passwordlastset
+Write-Host $last `n -ForegroundColor Magenta
+Write-Host @"
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⣤⣴⡶⠶⠾⠟⠛⠛⠛⠛⠷⠶⣦⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀       
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣶⠿⠛⠉⣁⣠⠤⠤⠀⠀⠀⠀⠀⠀⠀⠘⠷⢬⣙⡛⠷⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀      
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⡶⠟⢉⣠⠴⠚⠉⠁⣀⡀⠠⠀⠐⠂⠀⠀⠀⠀⠀⠀⠀⠀⠉⠓⠦⣍⡻⢷⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀       
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⠿⢋⡤⠞⢉⣠⡤⠖⢚⣉⣁⣤⣤⣴⣦⣤⣤⣄⣀⣀⠀⠀⠘⠲⢤⡀⠀⠈⠙⠲⣌⡛⢷⣤⡀⠀⠀⠀⠀⠀⠀⠀     
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⡾⢋⣡⠞⣋⠴⠚⣉⣥⡴⠾⠟⠛⢉⣉⣀⣠⣤⣤⣭⣍⣉⣛⡻⠶⣦⣤⣀⠉⠓⠦⣄⠀⠀⠙⢦⡉⠻⣦⡀⠀⠀⠀⠀⠀    
+⠀⠀⠀⠀⠀⠀⣀⣴⠟⢁⣴⣫⠷⢛⣡⣴⠟⠋⣁⣤⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠿⠿⣿⣶⣬⣝⡻⢶⣤⡀⠙⠂⢀⠀⠘⠢⡈⠻⣶⣄⠀⠀⠀   
+⠀⠀⠀⠀⣠⡾⠛⠁⢠⡿⠛⣡⣶⠟⢋⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣀⣤⣤⠀⠻⣿⣿⣿⣷⣮⣟⠷⣦⣜⠀⠀⠀⠀⠀⠈⠛⢷⣄⠀   
+⠀⢀⣰⡾⠋⠀⠀⠀⢋⣴⠿⢋⣦⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⡇⠈⣿⢿⣿⣿⣿⣿⣾⣝⡻⣦⣄⠀⠀⠀⠀⠀⠙⠷   
+⣰⡿⠋⠀⠀⠀⣠⡾⢛⣡⣶⡿⠛⣿⣿⣿⣿⠻⣿⠸⣏⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⡇⠀⣿⠈⢻⣿⢿⣿⢿⣿⣿⣷⣭⣀⠀⠀⠀⠀⠀⠀   
+⠀⠀⠀⠀⣤⣾⣯⣴⡿⠛⠋⠀⠀⢹⡼⣿⣿⠀⢿⡀⠉⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢣⡇⢀⡿⠀⣸⡏⢸⡟⢘⣿⠁⠙⠛⣻⣷⠀⢸⡆⠀⠀   
+⠀⠀⠀⠘⣯⣾⡟⢷⡄⠀⠀⠀⠀⠀⠀⠙⢿⣆⠘⣷⡀⠀⠀⠙⠿⣿⣿⣿⣿⣿⣿⣿⡿⢋⡾⢀⡼⠁⣰⡟⠀⠞⠁⢸⡏⠀⢀⣴⣿⠁⠀⣸⠇⠀⠀    
+⠀⠀⣰⣿⢛⣿⡇⠘⠃⠀⠀⠀⠀⠀⠀⠀⠈⠻⣧⣈⠃⠀⠀⠀⠀⠀⠈⠉⠛⠛⠋⠉⠴⠋⣠⠞⢀⣼⠏⠀⠀⠀⠀⡞⢀⣴⣿⣿⠏⠀⠀⠁⠀⠀⠀     
+⠀⠀⠙⠛⠛⠛⠿⠿⣷⣦⣄⡐⢦⣀⠀⠀⠀⠀⠈⠛⢷⣦⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⢠⢞⣡⣴⠟⠁⠀⠀⠀⢀⣠⣾⣿⣿⠟⡁⠀⠀⠀⠀⠀⠀⠀     
+⠀⠀⡀⠀⠀⠀⠀⠀⠀⠈⠙⠻⢶⣭⣟⡦⣤⣀⡀⠀⠀⠉⠛⠻⠿⠶⣶⣤⣤⣤⣶⣶⠿⠟⠋⠀⠀⠀⢀⣠⣶⣿⣿⡿⠋⣠⣾⠇⣠⠀⠀⠀⠀⠀⠀    
+⠀⠘⠛⠛⠛⠛⠻⢷⣦⣀⠀⠀⠀⠈⠉⠛⠷⢮⣽⣓⡲⠤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⣶⣿⣿⡿⠛⠉⢁⣴⠟⢡⡾⠃⠀⠀⠀⠀⠀⠀    
+⠀⠀⠀⠀⠀⠀⠸⣄⡈⠛⠿⣶⣤⣄⡀⠀⠀⠀⠀⠉⠙⠛⠿⠷⣶⣦⣤⣤⣤⣤⣴⣶⣶⡿⠿⠿⠟⠛⠋⠁⢀⣠⡾⠛⢁⡴⠋⣠⠆⢀⡴⠀⠀⠀⠀   
+⠀⠀⠀⠀⠀⠀⢻⣌⡛⠶⣤⣀⠈⠙⠛⠿⢶⣦⣤⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣾⠟⠋⣠⡶⢋⡠⠞⠁⠠⠋⠀⠀⠀⠀⠀     
+⠀⠀⠀⠀⠀⠀⠷⣍⡛⠳⢶⣭⠅⠀⠀⠀⠀⠀⠈⠉⢛⠛⠿⠷⢶⣶⣦⣤⣤⣤⣤⣤⣤⣴⣶⡾⠟⠛⠉⠀⠀⠚⠡⠖⣋⡤⠖⠉⠀⠀⠀⠀⠀⠀⠀    
+⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⠓⠃⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⣶⠦⠤⠤⠤⠤⣄⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀        
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣉⣓⣒⡒⠒⠶⠦⠀⢲⣄⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀        
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                     
+
+"@ -ForegroundColor white
+
+#check for flags that override normal process flow
+if ($contain -or ($reset -and $revoke)){
+    Contain
+    exit
+}
+
+if ($reset){
+    Reset-Pass
+    exit
+}
+if ($revoke){
+    Revoke-Sessions
+    exit
+}
+
+
+Write-Host "        <->        WATCHING FOR PASSWORD RESET!!        <->"`n
+While (1){
+    $new=(Get-ADUser $user -properties *).passwordlastset
+    $current=date
+    if ($last -ne $new){
+        write-host "Password has been updated as of "$new"!" -ForegroundColor Green
+        exit
+    } else {
+        if (($current-$start).Hours -eq 1){
+            write-host "Password was not reset after an hour. Password should be reset administratively!" -ForegroundColor Red
+            $rst = read-host "Would you like to try and reset it? (This is a good time to make sure you are elevated!) (y/n) "
+            if ($rst -eq "y" -or $rst -eq "Y"){
+                Reset-Pass
+                $rst2 = read-host "Would you also like to try to revoke sessions? (y/n)"
+                if ($rst2 -eq "y" -or $rst2 -eq "Y"){
+                    Revoke-Sessions
+                    exit
+                }
+                else{
+                    write-host "Password was reset, but sessions were not revoked!" -ForegroundColor Yellow
+                    exit
+                }
+            }
+            else{
+                write-host "Password was not reset and sessions were not revoked!" -ForegroundColor Red
+                exit
+            }
+        }
+        if ($verbose){
+            write-host "Password not changed as of -> " -NoNewline
+            write-host $current.TimeOfDay -ForegroundColor Red
+        }
+        sleep($rr);
+        $last=$new
+    }
+}
